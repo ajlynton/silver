b3b7cef4c86633807ed6769a12cf725b9c76bed9
Andrew Stoddart <astoddart@quantcast.com>
2018-12-29 17:24:19 +0000

    Initial version

---
+++ nielsen/main.py
@@ -0,0 +1,9 @@
+from src import mnist_loader
+import network
+
+
+if __name__ == '__main__':
+
+    training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+    net = network.Network([784, 30, 10])
+    net.SGD(training_data, epochs=30, mini_batch_size=10, eta=3.0, test_data=test_data)
